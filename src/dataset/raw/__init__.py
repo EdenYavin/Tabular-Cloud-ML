15c471d2dda157ed1e37a192fb61049e01228b98
BigDeadLock <eden.yavin25@gmail.com>
2024-08-26 17:14:39 +0300

    added bank marketing

--- src/dataset/raw/__init__.py
+++ src/dataset/raw/__init__.py
@@ -2,9 +2,11 @@ from src.dataset.raw.base import RawDataset
 from src.dataset.raw.heloc import HelocDataset
 from src.dataset.raw.adult import AdultDataset
 from src.dataset.raw.gesture_phase import GesturePhaseDataset
+from src.dataset.raw.bank_marketing import BankMarketing
 
 DATASETS = {
     HelocDataset.name: HelocDataset,
     GesturePhaseDataset.name: GesturePhaseDataset,
-    AdultDataset.name: AdultDataset
+    AdultDataset.name: AdultDataset,
+    BankMarketing.name: BankMarketing
 }

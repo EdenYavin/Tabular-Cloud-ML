4858933da2effb13a461e53e2ccc77071884e4dd
BigDeadLock <eden.yavin25@gmail.com>
2024-11-01 11:51:32 +0200

    for experiments

--- src/utils/config.py
+++ src/utils/config.py
@@ -47,8 +47,10 @@ class Config(BaseModel):
     neural_net_config: NEURAL_NET_CONFIG = NEURAL_NET_CONFIG()
     dataset_config: DatasetConfig = DatasetConfig(one_hot=True,
                                                   split_ratio=1,
-                                                  names=[DATASETS.HELOC, DATASETS.STUDENTS_DROPOUT,
-                                                         DATASETS.ADULT, DATASETS.BANK_MARKETING,
+                                                  names=[DATASETS.HELOC,
+                                                         DATASETS.ADULT,
+                                                         DATASETS.STUDENTS_DROPOUT,
+                                                         DATASETS.BANK_MARKETING,
                                                          DATASETS.GESTURE_PHASE
                                                          ]
                                                   )

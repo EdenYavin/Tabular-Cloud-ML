4896be1a2b12483d1e8808ce3b47aefb9e8b28ad
BigDeadLock <eden.yavin25@gmail.com>
2024-11-21 09:10:06 +0200

    add airline dataset

--- src/utils/config.py
+++ src/utils/config.py
@@ -46,7 +46,7 @@ class Config(BaseModel):
     neural_net_config: NEURAL_NET_CONFIG = NEURAL_NET_CONFIG()
     dataset_config: DatasetConfig = DatasetConfig(one_hot=True,
                                                   split_ratio=1,
-                                                  names=[DATASETS.LOAD_APPROVAL
+                                                  names=[DATASETS.AIRLINE_SATISFACTION
                                                          ]
                                                   )
     pipeline_config: PipelineConfig = PipelineConfig(force_to_create_again=True)

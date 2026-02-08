36b2d7dc90601f9bf9207aedba9a057225bcc339
eden-y_Sola <eden.y@sola.security>
2026-02-08 09:02:19 +0200

    feat(02-01): register FeatureAblationPipeline in pipeline package
    - Export FeatureAblationPipeline from src.pipeline
    - Add __all__ list with FeatureAblationPipeline, DeepSetFeatureEngineering, FrozenTContextExtractor
    - Enable import via 'from src.pipeline import FeatureAblationPipeline'

---
+++ src/pipeline/__init__.py
@@ -0,0 +1,9 @@
+from src.pipeline.feature_ablation_dataset import FeatureAblationPipeline
+from src.pipeline.deepset_features_dataset import DeepSetFeatureEngineering
+from src.pipeline.frozen_t_context_extractor import FrozenTContextExtractor
+
+__all__ = [
+    "FeatureAblationPipeline",
+    "DeepSetFeatureEngineering",
+    "FrozenTContextExtractor",
+]
